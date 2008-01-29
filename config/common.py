85ee46d6991ede7cfaa6d55864b44c4f6471c88e
Arif Otyakmaz <aoz@comnets.rwth-aachen.de>
2008-01-29 13:21:07 +0100

    Initial import

---
+++ config/common.py
@@ -0,0 +1,13 @@
+import os
+import CNBuildSupport
+from CNBuildSupport import CNBSEnvironment
+import wnsbase.RCS as RCS
+
+commonEnv = CNBSEnvironment(PROJNAME       = 'constanze',
+                            PROJMODULES    = ['TEST', 'BASE'],
+                            LIBRARY        = True,
+                            SHORTCUTS      = True,
+                            FLATINCLUDES   = False,
+			    REVISIONCONTROL = RCS.Bazaar('../', 'Constanze', 'unstable', '1.0'),
+                            )
+Return('commonEnv')
